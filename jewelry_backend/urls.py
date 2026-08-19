ac3320f999420a339793860d7f71d9d3b94f67a2
Divya Srilakshmi <borradivyasrilakshmi@gmai.com>
2026-08-19 12:36:07 +0530

    Add API URLs

--- jewelry_backend/urls.py
+++ jewelry_backend/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework_simplejwt.views import TokenRefreshView
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("products.urls")),
-    path("api/users/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
